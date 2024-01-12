0e00e05aaf8516d952e718663cc5e82ad29e1f74
Epttix <stijn@hansenolga.nl>
2024-01-12 17:07:32 +0100

    Update meesterproef.py
    erbij gezet wie wat gedaan heeft

--- meesterproef.py
+++ meesterproef.py
@@ -4,34 +4,34 @@ import time
 import os
 py.init()
 
-# beeldscherm variabele
+# beeldscherm variabele S
 hoogte = 500
 breedte = 800
 fps = 60
 win = py.display.set_mode((breedte, hoogte))
 py.display.set_caption("mooi spelletje")
 achtergrond_foto = py.image.load(os.path.join('achtergrond_foto.png'))
-#
+#algemene game variabele M
 game_state = 0
 running = True
 clock = 0
 index = 0
-# speler variabele
+# speler variabele L
 speler_x_y = speler_start_plek = [5, 445]
 oude_positie = speler_x_y
 speler_foto = py.image.load(os.path.join('speler_v1.png'))
-# bewegingsvariabele
+# bewegingsvariabele S
 activated = False
 last_key = py.K_w
 nieuwe_speler_positie = speler_x_y
-# muren
+# muren S
 wall_dimensions = [
     (breedte, 5), (breedte, 5), (5, hoogte), (5, hoogte), (150, 25), (25, 75)  , (25, 200)         , (50, 25), (225, 25) , (375, 25) , (25, 375)   , (25, 75) , (345, 25), (25, 300)
 ]
 wall_position = [
     (0, 0), (0, hoogte - 5), (0, 0), (breedte - 5, 0)     , (0, 350) , (150, 350), (300, hoogte-150), (250, 350) , (75, 275) , (0, 200) , (375, 200), (375, 0), (55, 75) , (600, 75)
 ]
-# spike
+# spike M
 spike_dimensies = [(10, 50)]
 spike_plek = [(375, 300)]
 #spike_foto = py.image.load(os.path.join('spike.png'))
@@ -40,30 +40,30 @@ spike_frame_2 = py.image.load(os.path.join('spike_frame 2.png'))
 spike_frame_1 = py.transform.rotate(spike_frame_1,90)
 spike_frame_2 = py.transform.rotate(spike_frame_2,90)
 spike_frames = [spike_frame_1, spike_frame_2]
-# teleporter
+# teleporter S
 teleporteer_binnen_dimensies = [(50, 5), (5, 50)    , (5, 50)]
 teleporteer_binnen_locatie = [(325, 495), (600, 200), (620, 200)]
 teleporteer_buiten_dimensies = [(50, 5), (5, 50)    , (5, 50)]
 teleporteer_buiten_locatie = [(325, 0), (620, 200)  , (600, 200)]
-# vijanden
+# vijanden L
 vijanden_locatie = [[400, 5]  , [400, 200]]
 vijanden_dimensies = [(50, 50), (50, 50)]
 beweeg_richting = ["rechts", "rechts"]
 vijanden_foto = py.image.load(os.path.join('geestje_V3.png'))
-#coin
+#coin S
 coin = []
 coin_aantal = 0
 coin_foto = py.image.load(os.path.join('Coin_25px25px.png'))
 coin_locatie = [(745, 450)]
 coin_dimensies = [(25,25)]
-coin_deactivatie = [-1,]
+coin_deactivatie = [-1, ]
 for i in range(len(coin_locatie)):
     coin.append(py.Rect(coin_locatie[i], coin_dimensies[i]))
 # eind zone
 eindzone_locatie = (745,0)
 eindzone_dimensies = (50,5)
 
-
+#functie om de animaties te cycelen -M
 def cycle_animaties():
     global clock, index, frame
 
@@ -84,7 +84,7 @@ def cycle_animaties():
     if clock == 60:
         clock = 0
     return frame
-#start up_scherm
+#start up_scherm S
 
 def start_up_scherm():
     global tekst_rect, tekst, win
@@ -95,7 +95,7 @@ def start_up_scherm():
     py.display.update()
 
 
-#eindstand scherm
+#eindstand scherm L
 def eindstand_scherm():
     win.fill((255,255,255))
 
@@ -103,7 +103,7 @@ def eindstand_scherm():
     font.render_to(win, (20,200), "Je hebt het gehaald woe hoee", (255, 0, 255))
     py.display.update()
 
-# tekent het scherm
+# tekent het scherm S
 
 
 def draw_scr():
@@ -116,29 +116,29 @@ def draw_scr():
     skip = -1
     #
     spike_cycle_frame = cycle_animaties()
-    # maakt de achtergrond
+    # maakt de achtergrond S
     win.fill((0, 0, 0))
     win.blit(achtergrond_foto, (0,0))
-    # foto van de speler
+    # foto van de speler S
     win.blit(speler_foto, (speler_x_y[0], speler_x_y[1]))
-    # speler vierkant
+    # speler vierkant S
     speler_rect = py.Rect(speler_x_y[0], speler_x_y[1], 50, 50)
-    # muren
+    # muren S
     for i in range(len(wall_dimensions)):
         muren.append(py.draw.rect(win, (255, 255, 0), (wall_position[i], wall_dimensions[i])))
-    # de spikes
+    # de spikes S
     for i in range(len(spike_dimensies)):
 
         win.blit(spike_cycle_frame, (spike_plek[i]))
         #spike.append(py.draw.rect(win, (0, 0, 255), (spike_plek[i], spike_dimensies[i])))
         spike.append(py.Rect((spike_plek[i], spike_dimensies[i])))
-    # de teleporteer ingang
+    # de teleporteer ingang M
     for i in range(len(teleporteer_binnen_locatie)):
         teleporteer_binnen.append(py.draw.rect(win, (255, 255, 0), (teleporteer_binnen_locatie[i], teleporteer_binnen_dimensies[i])))
-    # de teleporteer uitgang
+    # de teleporteer uitgang M
     for i in range(len(teleporteer_buiten_locatie)):
         teleporteer_buiten.append(py.draw.rect(win, (0, 255, 0), (teleporteer_buiten_locatie[i], teleporteer_buiten_dimensies[i])))
-    # de vijanden
+    # de vijanden L
     for i in range(len(vijanden_locatie)):
         vijanden.append(py.Rect(vijanden_locatie[i], vijanden_dimensies[i]))
 
@@ -146,7 +146,7 @@ def draw_scr():
         win.blit(vijanden_foto, vijanden_locatie[i])
     eindzone_rect = py.draw.rect(win, (125, 255, 75), (eindzone_locatie, eindzone_dimensies))
 
-
+    # coin S
     for i in range(len(coin_deactivatie)):
         for ii in range(len(coin_locatie)):
             if coin_deactivatie[i] == ii:
@@ -157,7 +157,7 @@ def draw_scr():
         win.blit(coin_foto, coin_locatie[i])
 
 
-
+    # oude code niet gebruikt S
     # for i in range(len(coin_deactivatie)):
     #
     #     for j in range(len(coin_locatie)):
@@ -173,7 +173,7 @@ def draw_scr():
     # ververst het scherm
     py.display.update()
 
-# functie voor het bewegen van de speler
+# functie voor het bewegen van de speler S
 def moving(object_pos, object_velocity, direction):
     final_position = [0, 0]
     # kijkt naar welke kant de speler beweegt
@@ -192,7 +192,7 @@ def moving(object_pos, object_velocity, direction):
             final_position[0] = object_pos[0] - object_velocity
     return final_position
 
-# functie voor de beweging van de vijand
+# functie voor de beweging van de vijand S
 def vijand_beweging():
     # zet de speed van de vijanden
     speed = 3
@@ -234,7 +234,7 @@ def vijand_beweging():
                     vijanden_locatie[i][1] = oude_locatie[1]
                     beweeg_richting[i] = "rechts"
 
-# kijkt of object botst met teleporters
+# kijkt of object botst met teleporters M
 def botsing_teleporteren(object_rect, tele_in, object_x_y):
     tele_hit = object_rect.collidelist(tele_in)
     object_plek = []
@@ -246,12 +246,12 @@ def botsing_teleporteren(object_rect, tele_in, object_x_y):
         object_plek = object_x_y
     return object_plek, tele_hit
 
-# checkt of object met muur botst
+# checkt of object met muur botst M
 def botsing_muren(object_rect):
     wallhit = object_rect.collidelist(muren)
     return wallhit
 
-# checkt of object met vijand botst, komt veel overeen met botsing_spikes
+# checkt of object met vijand botst, komt veel overeen met botsing_spikes L
 def botsing_vijand(object_rect, running_import):
     vijand_hit = object_rect.collidelist(vijanden)
     if vijand_hit > -1:
@@ -260,7 +260,7 @@ def botsing_vijand(object_rect, running_import):
         running = running_import
     return running
 
-# checkt of object met de spike botst komt veel overeen met botsing_vijand
+# checkt of object met de spike botst komt veel overeen met botsing_vijand S
 def botsing_spikes(object_rect, running_import):
     spike_hit = object_rect.collidelist(spike)
     if spike_hit > -1:
@@ -269,7 +269,7 @@ def botsing_spikes(object_rect, running_import):
         running = running_import
     return running
 
-
+# checkt of object met een coin botst S
 def botsing_coin(object_rect, coin_aantal_import):
     coin_hit = object_rect.collidelist(coin)
     coin_aantal = coin_aantal_import
@@ -279,7 +279,7 @@ def botsing_coin(object_rect, coin_aantal_import):
         return coin_aantal, coin_hit
 
     return coin_aantal, -1
-
+# botsing met de eind zone L
 def botsing_eind_zone(object_rect):
     eindhit = object_rect.colliderect(eindzone_rect)
     if eindhit == True:
@@ -287,7 +287,7 @@ def botsing_eind_zone(object_rect):
     else:
         return 0
 
-# de main game functie
+# de main game functie S
 def main():
     # stelt aantal variabele op
     global speler_x_y, moving_direction, oude_positie, activated, game_state, coin_aantal, coin_deactivatie
@@ -431,10 +431,10 @@ def main():
 
 
     return "dood"
-
+# zorgt ervoor dat de goede main word uitgevoerd hier kwam max achter M
 if __name__ == "__main__":
     reden = main()
-
+# zorgt ervoor dat als je niet dood bent dat je opnieuw kan proberen L
 while reden != "gestopt":
     if reden == "dood":
         game_state = 0
